92fe8c98837c53061666cc46a9e3ad3b8f699900
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2020-01-23 19:01:06 +0530

    Updating command line tool for testing

---
+++ src/lib/index.ts
@@ -0,0 +1,213 @@
+// defineUser_local(userId, [{groupName, groupId}])
+// setUserGroup_local(userId, groupName, groupId)
+// defineExperiment_server(name, [{id, point}], [conditions], unitOfAssignment, consistencyRule, postExperimentRule, group)
+// setExperimentStatus_server(name, state)
+// getAllExperimentConditions_client(userId, userEnvironment)
+// getExperimentCondition_client(experimentCondition, experimentPoint, experimentId)
+// markExperimentPoint_client(userId, userEnvironment, experimentPoint, experimentId)
+// validateAssignment_local(condition1, condition2)
+// deleteExperiment_server(name)
+
+import {
+  ASSIGNMENT_UNIT,
+  CONSISTENCY_RULE,
+  POST_EXPERIMENT_RULE,
+  EXPERIMENT_STATE,
+} from 'ees_types';
+import fetch from 'node-fetch';
+
+interface IUser {
+  id: string;
+  group: any;
+}
+
+export enum Validation {
+  Equal = 'equal',
+  NotEqual = 'notEqual',
+}
+
+// const baseUrl = `https://ees-backend.herokuapp.com/api/`;
+const baseUrl = `http://localhost:3030/api/`;
+
+export function defineUser_local(userId: string, userEnvironment: any): IUser {
+  return {
+    id: userId,
+    group: userEnvironment,
+  };
+}
+
+export function setUserGroup_local(
+  userDef: IUser,
+  groupName: string,
+  groupId: string
+): IUser {
+  userDef.group[groupName] = groupId;
+  return userDef;
+}
+
+interface ISegmentDefinition {
+  id: string;
+  point: string;
+}
+export async function defineExperiment_server(
+  name: string,
+  segmentDefinition: ISegmentDefinition[],
+  conditions: string[],
+  unitOfAssignment: ASSIGNMENT_UNIT,
+  consistencyRule: CONSISTENCY_RULE,
+  postExperimentRule: POST_EXPERIMENT_RULE,
+  group?: string
+) {
+  const url = `${baseUrl}experiments`;
+  const postData = JSON.stringify({
+    name: name,
+    description: '',
+    startOn: null,
+    consistencyRule,
+    assignmentUnit: unitOfAssignment,
+    postExperimentRule,
+    state: 'inactive',
+    tags: [],
+    group: group || undefined,
+    conditions: conditions.map(condition => {
+      return {
+        assignmentWeight: 1 / conditions.length,
+        conditionCode: condition,
+      };
+    }),
+    segments: segmentDefinition.map(segmentInd => ({
+      id: `${segmentInd.id}_${segmentInd.point}`,
+      point: segmentInd.point,
+      name: segmentInd.id,
+      description: '',
+    })),
+  });
+
+  const result = await fetch(url, {
+    method: 'POST',
+    body: postData,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+
+  return result;
+}
+
+async function getAllExperiment() {
+  const url = `${baseUrl}experiments`;
+  const result = await fetch(url).then(res => res.json());
+  return result;
+}
+
+export async function setExperimentStatus_server(
+  name: string,
+  state: EXPERIMENT_STATE
+) {
+  // get all experiment
+  const allExperiment = await getAllExperiment();
+  const experiment: any = allExperiment.find(
+    (experiment: any) => experiment.name === name
+  );
+  const url = `${baseUrl}state`;
+  const postData = JSON.stringify({
+    experimentId: experiment.id,
+    state,
+  });
+  const result = await fetch(url, {
+    method: 'PUT',
+    body: postData,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+
+  return result;
+}
+
+export async function deleteExperiment_server(name: string) {
+  const allExperiment = await getAllExperiment();
+  const experiment: any = allExperiment.find(
+    (experiment: any) => experiment.name === name
+  );
+  if (experiment) {
+    const url = `${baseUrl}experiments/${experiment.id}`;
+    const result = await fetch(url, { method: 'DELETE' }).then(res =>
+      res.json()
+    );
+    return result;
+  }
+}
+
+export async function getAllExperimentConditions_client(user: IUser) {
+  const url = `${baseUrl}assign`;
+  const postData = JSON.stringify({
+    userId: user.id,
+    userEnvironment: user.group,
+  });
+
+  const result = await fetch(url, {
+    method: 'POST',
+    body: postData,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+
+  return result;
+}
+
+export function getExperimentCondition_client(
+  experimentCondition: any,
+  experimentPoint: string,
+  experimentId: string
+) {
+  const expCondition = experimentCondition.find((condition: any) => {
+    return (condition.name =
+      experimentId && condition.point === experimentPoint);
+  });
+  return (
+    (expCondition && expCondition.assignedCondition.conditionCode) || 'default'
+  );
+}
+
+export async function markExperimentPoint_client(
+  experimentPoint: string,
+  experimentId: string,
+  user: IUser
+) {
+  const postData = JSON.stringify({
+    experimentId,
+    experimentPoint,
+    userId: user.id,
+    userEnvironment: user.group,
+  });
+
+  const url = `${baseUrl}mark`;
+  const result = await fetch(url, {
+    method: 'POST',
+    body: postData,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+  return result;
+}
+
+export async function validate_local(
+  condition1: string,
+  condition2: string,
+  validation: Validation,
+  validationName?: string
+) {
+  switch (validation) {
+    case Validation.Equal:
+      return condition1 === condition2
+        ? console.log('\x1b[32m', `${validationName} Passed`)
+        : console.log('\x1b[31m', `${validationName} Failed`);
+    case Validation.NotEqual:
+      return condition1 !== condition2
+        ? console.log('\x1b[32m', `${validationName} Passed`)
+        : console.log('\x1b[31m', `${validationName} Failed`);
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
-var path = require("path");
+const path = require('path');
+const fs = require('fs');
+const scriptPath = 'src/scripts/';
+
+const entryPoint = fs
+  .readdirSync(path.join(__dirname, scriptPath))
+  .reduce((accumulator, scriptName) => {
+    const name = path.parse(scriptName).name;
+    accumulator[name] = `./${scriptPath}${name}`;
+    return accumulator;
+  }, {});
 
 module.exports = {
-  entry: "./src/index.ts",
-  target: "node",
-  mode: "development",
-  devtool: "inline-source-map",
+  entry: entryPoint,
+  target: 'node',
+  mode: 'development',
+  devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
   },
   resolve: {
-    extensions: [".ts", ".js"] //resolve all the modules other than index.ts
+    extensions: ['.ts', '.js'], //resolve all the modules other than index.ts
   },
   module: {
     rules: [
       {
-        use: "ts-loader",
-        test: /\.ts?$/
-      }
-    ]
-  }
+        use: 'ts-loader',
+        test: /\.ts?$/,
+      },
+    ],
+  },
 };
