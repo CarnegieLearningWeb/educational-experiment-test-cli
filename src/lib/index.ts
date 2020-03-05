7124439a720b6c349fe197b7e1b747d7d790a0aa
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2020-03-05 16:30:16 +0530

    Adding development API link over here

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -28,7 +28,7 @@ export enum Validation {
   NotEqual = 'notEqual',
 }
 
-const baseUrl = `http://localhost:3030/api/`;
+const baseUrl = `http://upgrade-development.us-east-1.elasticbeanstalk.com/api/`;
 
 export async function init_client(
   userId: string,
